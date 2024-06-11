b55f396aeb557677597388fdd68512e03266bc25
murenz1 <162572714+murenz1@users.noreply.github.com>
2024-06-11 02:50:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const overlay = document.getElementById('overlay');
+
+let popup = document.getElementById('popup');
+
+function openPopup(){
+  popup.classList.add('open-popup')
+}
+
+function closePopup(){
+  popup.classList.remove('open-popup')
+}
+
+overlay.addEventListener('open-popup' ,() => {
+  closePopup();
+});
+
+
+
